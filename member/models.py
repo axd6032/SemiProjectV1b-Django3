5cb101fe00d9bd71424a28dda283ca23a80bf730
axd6032 <xlfkfntm@naver.com>
2022-06-23 12:04:12 +0900

    Django3 views, template 수정 - 클라이언트/서버 측 유효성 검사, Member 객체 생성후 디비저장, 가입완료시 ok페이지로 전환

--- member/models.py
+++ member/models.py
@@ -3,13 +3,16 @@ import datetime
 from django.db import models
 
 # Create your models here.
+
+# Django 에서 폼 데이터를 다루는 방법은 크게 2가지 - Model, ModelForm
+# member 테이블의 구조와 유사하게 member 모델 정의
 class Member(models.Model):
     id = models.AutoField(primary_key=True)
     userid = models.CharField(max_length=18, unique=True)
     passwd = models.CharField(max_length=18)
     name = models.CharField(max_length=10)
     email = models.CharField(max_length=100)
-    regdate = models.DateTimeField(default=datetime.now)
+    regdate = models.DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         db_table = 'member'  # 생설할 테이블 이름 지정
